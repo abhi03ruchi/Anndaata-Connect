639aaee86dd953802a274b090f23b424edaa872f
Harshita099 <harshitaprachi1523@gmail.com>
2024-01-28 00:18:41 +0530

    Make SignUp Page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home/Home';
 import Scroll  from './Components/GoToTop/GoToTop';
+import RegisterAndLogin from './Pages/Home/register/RegisterAndLogin';
 
 function App() {
   return (
@@ -9,6 +10,9 @@ function App() {
        <Scroll />
       <Routes>
           <Route path="/" element={<Home />} />
+        </Routes>
+        <Routes>
+          <Route path="/login" element={<RegisterAndLogin/>} />
         </Routes>
     </div>
   );
