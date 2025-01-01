aed4004e1a690e191d834038227ce631057906dd
AnkitShaw_100 <ankitshaw6933@gmail.com>
2025-01-01 16:39:21 +0530

    made login page responsive and added logo in it

--- src/Pages/RegisterAndLoginforNGO/Login.jsx
+++ src/Pages/RegisterAndLoginforNGO/Login.jsx
@@ -5,6 +5,7 @@ import { Link, useNavigate } from "react-router-dom";
 import "./RegisterAndLogin.css";
 import { useUserAuth } from "../../context/UserAuthContext";
 import { Alert } from "react-bootstrap";
+import  logo  from "../../Components/assets/logo.png";
 import GoogleButton from "react-google-button";
 
 
@@ -38,12 +39,8 @@ const Login = () => {
 
   return (
       <div className="loginstyle  flex flex-1 flex-col justify-center">
-        <div className=" sm:mx-auto sm:w-full sm:max-w-sm">
-          <img
-            className="mx-auto h-10 w-auto"
-            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-            alt="Your Company"
-          />
+        <div className="logoImage">
+          <img className="mx-auto h-10 w-auto" src={logo} alt="Your Company" />
           <h2 className="mt-10 text-center text-5xl font-bold leading-9 tracking-tight text-gray-900">
             Login in
           </h2>
