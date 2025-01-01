8e2ea2609b01e4a73256cd7c49cd909872c21aec
AnkitShaw_100 <ankitshaw6933@gmail.com>
2025-01-01 16:44:55 +0530

    changed made in register page from footer section add logo and made responsive

--- src/Pages/RegisterAndLoginforNGO/Registration.jsx
+++ src/Pages/RegisterAndLoginforNGO/Registration.jsx
@@ -7,6 +7,7 @@ import "./RegisterAndLogin.css";
 import { useNavigate } from 'react-router-dom';
 import { Link } from "react-router-dom";
 import { Alert } from "react-bootstrap";
+import  logo  from "../../Components/assets/logo.png";
 import { useUserAuth } from "../../context/UserAuthContext";
 
 const Registration = () => {
@@ -33,11 +34,7 @@ const Registration = () => {
     <div className='loginstyle' >
       <div className="flex flex-1 flex-col justify-center ">
         <div className=" sm:mx-auto sm:w-full sm:max-w-sm">
-          <img
-            className="mx-auto h-10 w-auto"
-            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-            alt="Your Company"
-          />
+          <img className="mx-auto h-10 w-auto" src={logo} alt="Your company" />
           <h2 className="mt-10 text-center text-4xl font-bold leading-9 tracking-tight text-gray-900">
             Register
           </h2>
