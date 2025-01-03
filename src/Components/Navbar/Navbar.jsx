ab557473e8f147b5e8b7efcaa79817b113b3a144
Sowdarjya <kolaysowdarjya@gmail.com>
2025-01-03 09:56:02 +0530

    Fixed home page responsiveness

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,33 +1,30 @@
-import {  useState } from 'react'
-import { Dialog,  Popover } from '@headlessui/react'
-import {
-  Bars3Icon,
-  XMarkIcon,
-} from '@heroicons/react/24/outline'
-import Button from './Button'
+import { useState } from "react";
+import { Dialog, Popover } from "@headlessui/react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import Button from "./Button";
 
-import Logo from '../assets/logo.png'
-
+import Logo from "../assets/logo.png";
 
 function classNames(...classes) {
-  return classes.filter(Boolean).join(' ')
+  return classes.filter(Boolean).join(" ");
 }
 
 export default function Example() {
-  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   return (
     <header className="bg-white">
-      <nav className="mx-auto flex max-w-7xl items-center justify-between p-1 lg:px-8" aria-label="Global">
+      <nav
+        className="mx-auto flex max-w-7xl items-center justify-between p-1 lg:px-8"
+        aria-label="Global"
+      >
         <div className="flex lg:flex-1">
           <a href="#" className="-m-1.5 p-1.5">
             <span className="sr-only">Your Company</span>
-            <img
-              style={{ height: '90px' }}
-              src={Logo} alt="" />
+            <img src={Logo} alt="" className="h-16" />
           </a>
         </div>
-        <div className="flex lg:hidden">
+        <div className="flex lg:hidden z-50">
           <button
             type="button"
             className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
@@ -41,21 +38,35 @@ export default function Example() {
           <a href="/" className="text-lg  leading-6 text-[#28183b] font-sans">
             Home
           </a>
-          <a href="#about" className="text-lg  leading-6 text-[#28183b] font-sans">
+          <a
+            href="#about"
+            className="text-lg  leading-6 text-[#28183b] font-sans"
+          >
             About
           </a>
-          <a href="/services" className="text-lg  leading-6 text-[#28183b] font-sans">
+          <a
+            href="/services"
+            className="text-lg  leading-6 text-[#28183b] font-sans"
+          >
             Services
           </a>
-          <a href="/team" className="text-lg  leading-6 text-[#28183b] font-sans">
+          <a
+            href="/team"
+            className="text-lg  leading-6 text-[#28183b] font-sans"
+          >
             Team
           </a>
         </Popover.Group>
         <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-         <Button/>
+          <Button />
         </div>
       </nav>
-      <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
+      <Dialog
+        as="div"
+        className="lg:hidden"
+        open={mobileMenuOpen}
+        onClose={setMobileMenuOpen}
+      >
         <div className="fixed inset-0 z-10" />
         <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
@@ -79,7 +90,7 @@ export default function Example() {
           <div className="mt-6 flow-root">
             <div className="-my-6 divide-y divide-gray-500/10">
               <div className="space-y-2 py-6">
-            <a
+                <a
                   href="/"
                   className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                 >
@@ -117,5 +128,5 @@ export default function Example() {
         </Dialog.Panel>
       </Dialog>
     </header>
-  )
+  );
 }
--- src/Components/SustainableGoals.jsx
+++ src/Components/SustainableGoals.jsx
@@ -1,31 +1,29 @@
-import React from 'react'
-import img1 from '../Components/assets/2.png';
-import img2 from '../Components/assets/3.png';
-import img3 from '../Components/assets/12.png';
-import img4 from '../Components/assets/13.png';
+import React from "react";
+import img1 from "../Components/assets/2.png";
+import img2 from "../Components/assets/3.png";
+import img3 from "../Components/assets/12.png";
+import img4 from "../Components/assets/13.png";
 
 export default function SustainableGoals() {
-    return (
-      <div className="bg-white py-24 sm:py-32">
-        <div className="mx-auto max-w-7xl px-6 lg:px-8">
-          <h2 className="text-center text-3xl font-semibold leading-8 text-gray-900">
-            Sustainable Development Goals
-          </h2>
-          <div className="mx-auto mt-10 flex max-w-full flex-row items-center justify-center gap-x-8 gap-y-10">
-            <img
-              className="col-span-2  w-lg object-contain lg:col-span-1"
-              src={img1}
-              alt="Transistor"
-             
-            />
-            <img
-              className="col-span-2  w-lg object-contain lg:col-span-1"
-              src={img3}
-              alt="Tuple"
-            />
-          </div>
+  return (
+    <div className="bg-white py-24 sm:py-32">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <h2 className="text-center text-3xl font-semibold leading-8 text-gray-900">
+          Sustainable Development Goals
+        </h2>
+        <div className="mx-auto mt-10 flex max-w-full md:flex-row flex-col items-center justify-center gap-x-8 gap-y-10">
+          <img
+            className="col-span-2  w-lg object-contain lg:col-span-1"
+            src={img1}
+            alt="Transistor"
+          />
+          <img
+            className="col-span-2  w-lg object-contain lg:col-span-1"
+            src={img3}
+            alt="Tuple"
+          />
         </div>
       </div>
-    )
-  }
-  
+    </div>
+  );
+}
