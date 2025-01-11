a8627301713e857b258153763e555f6fc7ca4e23
Yasir Akhlaque <akhlaqueyasir28@gmail.com>
2025-01-11 16:12:53 +0530

    Added New Register page for user

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       fontFamily: {
         sans: ['Poppins', 'sans-serif'],
       },
+      animation: {
+        'spin-slow': 'spin 2s linear infinite',
+      }
     },
   },
   plugins: [
     require('tailwind-scrollbar'),
-],
+    require("tailwindcss-animate")
+  ],
 }
